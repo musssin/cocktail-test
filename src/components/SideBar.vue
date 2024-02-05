7c2bbc3e074ccbd5a280f7c9ed8451301b7565ac
Mussin Y <mussin0401@gmail.com>
2024-02-05 23:28:30 +0600

    added components

---
+++ src/components/SideBar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="sidebar">
+
+    <nav>
+      <RouterLink v-for="item of menu" :key="item" :to="{ name: 'cocktail-info', params: { cocktailName: item } }">{{ item
+      }}
+      </RouterLink>
+    </nav>
+
+  </div>
+</template>
+
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+const menu = [
+  'margarita',
+  'mojito',
+  'a1',
+  'kir'
+]
+</script>
+
+
+
+<style scoped></style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,22 +1,26 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import CocktailInfo from '../views/CocktailInfo.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: '',
+      redirect: '/margarita',
+      children: [
+        {
+          path: '/:cocktailName',
+          name: 'cocktail-info',
+          component: CocktailInfo
+        }
+      ]
     },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
+    // {
+    //   path: '*',
+    //   name: 'error',
+    //   component: () => import('../views/Error.vue')
+    // }
   ]
 })
 
